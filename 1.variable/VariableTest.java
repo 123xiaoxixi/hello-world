8027d4b5ed11417e200c04cfab94a41d1d4ea830
123xiaoxixi <1280299664@qq.com>
2022-05-08 22:43:19 +0800

    add folder for variable test

---
+++ 1.variable/VariableTest.java
@@ -0,0 +1,16 @@
+public class VariableTest{
+    public static void main(String[] args){
+        int a = 10;
+        byte b1 = -128;
+        byte b2 = 127;
+        long a1 = 123456L;
+        float f = 123.1F;
+        double d = 123.2;
+        System.out.println(a);
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(f+1);
+        System.out.println(b2);
+        System.out.println(a1);
+    }
+}
