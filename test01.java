85c537b70c7e9b25ca4d65af2bfa5085d22f80c2
123xiaoxixi <1280299664@qq.com>
2022-05-08 22:20:41 +0800

    add:test01.java

---
+++ test01.java
@@ -0,0 +1,11 @@
+/**
+ * @author shirong
+ * @version 1.0.0
+ * @function HelloWorld
+ */
+public class test01{
+    //main 方法
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
