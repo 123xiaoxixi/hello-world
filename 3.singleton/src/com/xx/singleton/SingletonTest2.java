a62208992f4d25518bdec937689c67bb0025e522
123xiaoxixi <1280299664@qq.com>
2022-06-11 20:08:30 +0800

    modify:fix thread security

--- 3.singleton/src/com/xx/singleton/SingletonTest2.java
+++ 3.singleton/src/com/xx/singleton/SingletonTest2.java
@@ -8,17 +8,29 @@ public class SingletonTest2 {
 	}
 }
 /*
- * 单例模式:懒汉式
+ * 懒汉式
  */
 class Earth{
 	private Earth() {
 		
 	}
 	private static Earth instance = null;
+	//方式一:
 	public static Earth getInstance() {
-		if(instance == null) {
-			instance = new Earth();
+		if(instance ==null) {
+			synchronized (Earth.class) {
+				if(instance == null) {
+					instance = new Earth();
+				}
+			}	
 		}
 		return instance;
 	}
+	//方式二:
+//	public static synchronized Earth getInstance() {
+//		if(instance == null) {
+//			instance = new Earth();
+//		}
+//		return instance;
+//	}
 }
